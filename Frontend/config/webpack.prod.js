8b5925fc9de02cc914734f9345f143ef2172197b
jwu-ice <sju02048@naver.com>
2023-01-01 20:13:25 +0900

    Add: webpack, react, typescript, tailwind 프로젝트 구성

---
+++ Frontend/config/webpack.prod.js
@@ -0,0 +1,57 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "cheap-module-source-map",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "./",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          sourceMap: true,
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    runtimeChunk: true,
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: "vendor",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+});
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="p-4 bg-slate-500">xssssaassd</div>;
+};
+
+export default App;
